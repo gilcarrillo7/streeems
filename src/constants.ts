c7d1d238adf563544f874bb7f1c1c2d9aa5e7bb2
gilcarrillo7 <gilcarrillo7@gmail.com>
2023-07-12 11:56:51 -0600

    master branch

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `https://cors-anywhere.herokuapp.com/https://www.streeems.com/api/v1`;
+export const BASE_URL = `https://www.streeems.com/api/v1`;
 export const PUBLICATIONS = "publications";
 export const DOSSIERS = "dossiers";
 export const INSTITUTIONS = "institutions";
